8e15730c747652b027769e035e763b882aff27c0
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-04-15 15:16:37 +0300

    debugging outputs

--- public/main.js
+++ public/main.js
@@ -1,12 +1,3 @@
-function renderImg(data) {
-    data.forEach(function (imgSrc) {
-        var imgEl = document.createElement('img');
-        imgEl.src = imgSrc;
-        document.getElementById('data').appendChild(imgEl);
-    });
-}
-
-
 document.getElementById("search_button").addEventListener('click', async event => { // on-click event 
     event.preventDefault(); // prevent reloading page
     const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/'); //get date of search
@@ -16,8 +7,7 @@ document.getElementById("search_button").addEventListener('click', async event =
     const response = await request.json(); // response of endpoint
     const gifs = response.gif;
     const stickers = response.sticker
-   
-console.log(gifs.data)
+
     // allocate all picture urls that has type gif
 
     let gif_url = gifs.data.reduce((acc, {url, title}) => {
@@ -31,48 +21,26 @@ console.log(gifs.data)
 
     }, {});
 
-    let sticker_url = stickers.data.reduce((acc, {url, title}) => {
-        if (!url || !title) {
+    let sticker_url = stickers.data.reduce((acc, {url}) => {
+        if (!url) {
             return acc;
         }
         return {
             ...acc,
-            [title]: url
+        url
+            
             
         };
 
     }, {});
-
-    function handleJSON(json){
-        if (!json.data) {
-            return;
-        }
+    let stick = [sticker_url];
+    stick.forEach((img)=>{
+        console.log(img)
+    })
+  
     
-        var result = [],
-            counter = json.data.length;
-    
-        while(counter--){
-            try {
-                result.push(json.data[counter].images.original.url);
-            } catch {
-                console.log('An error has occurred while processing GIHPY response');
-            }	
-        }
-    
-        return result;
-    }
- 	var result = [],
-		counter = gifs.data.length;
+console.log(response)
 
-	while(counter--){
-		try {
-			result.push(gifs.data[counter].images.original.url);
-		} catch {
-			console.log('An error has occurred while processing GIHPY response');
-		}	
-	}
-
-	
     const post = {
             date,
             searchTerm,
@@ -92,3 +60,4 @@ console.log(gifs.data)
     const db_json = await db_response.json();
     console.log(db_json);
 });
+
--- server.js
+++ server.js
@@ -2,15 +2,40 @@ const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const Datastore = require('nedb');
+const mongoose = require('mongoose');
 
+//database connection
+mongoose.connect('mongodb://localhost:27017/learning-mongodb', {useNewUrlParser: true});
+
+const db = mongoose.connection;  // mongoose connection 
+db.on('error', (err)=>{
+    console.log('error has occurred with database ' + err);
+})
+db.on('open', ()=>{
+    console.log('connection to db success');
+});
+//schemas
+const Schema = mongoose.Schema;
+
+const gifSchema = new Schema({
+    date: String,
+    searchTerm: String,
+    gif_url: Object,
+    sticker_url: Object
+    
+});
+
+const Gif = mongoose.model('Gif', gifSchema);
+
+
+
+// endpoints starts here
 app.use("/public", express.static('./public/'));
 app.use(express.json({
     limit: '1mb'
 }));
 
-const database = new Datastore('search.db');
-database.loadDatabase();
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
@@ -19,9 +44,19 @@ app.post('/api', async (req, res) => {
     console.log("POST - DONE")
     console.log(req.body)
     const response = await req.body;
-    database.insert(response);
+    const date = await response.date;
+    const searchTerm = await response.searchTerm
+    const gif_url = await response.gif_url
+    const sticker_url = await response.sticker_url
+
+    const newGif = new Gif
+    newGif.date = date
+    newGif.searchTerm = searchTerm
+    newGif.gif_url = gif_url
+    newGif.sticker_url = sticker_url
+    newGif.save();
     res.json({
-        response: response
+        response: response,
     });
 });
 
@@ -37,16 +72,17 @@ app.get('/search/:query', async (req, res) => {
         const sticker_url = 'http://api.giphy.com/v1/stickers/search?api_key=' + api_key;
         const fetch_sticker = await fetch(sticker_url + '&q=' + search)
         const resp_sticker = await fetch_sticker.json();
-        if(!resp_sticker) {
-            resp_sticker === null
-        }
+      
         const data = {
             gif: resp_gif,
             sticker: resp_sticker
         }
+        
 
-        res.json(data);
-   
+    res.json(data);
+    console.log("ok")
+    
+
 });
 app.listen(PORT, function () {
     console.log("listening port 5000");
